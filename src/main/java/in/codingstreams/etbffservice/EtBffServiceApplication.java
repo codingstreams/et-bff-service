fd3f5147449dc263609b25015fb15648210772dc
Coding Streams <akxhdev@outlook.com>
2024-02-04 22:28:05 +0530

    Initial Commit

---
+++ src/main/java/in/codingstreams/etbffservice/EtBffServiceApplication.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etbffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtBffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtBffServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/codingstreams/etbffservice/EtBffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etbffservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtBffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
